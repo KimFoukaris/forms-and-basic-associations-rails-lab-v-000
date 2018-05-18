b99eff7f49651eaa2e62ddcd5ad6bbdc08fa5ce6
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-18 16:19:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:artist_name, :title, :genre_id, note_content: [])
+    params.require(:song).permit(:artist_name, :title, :genre_id, note_ids: [])
   end
 end
