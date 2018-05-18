0fd2c989142f9c55aab5297c28bf010133338bfc
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-18 16:56:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(notes)
     notes.each do |note|
       n = Note.find_or_create_by(content: note)
-      self.notes << note
+      self.notes < n
     end
   end
 
